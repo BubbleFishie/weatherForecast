648762d715edbd9cd9c63c31d896ace0e5571301
Bubble_Fish <2020244147@tju.edu.cn>
2022-08-10 22:15:49 +0800

    加入数据库和表

---
+++ app/src/main/java/com/example/weatherforecast/db/City.java
@@ -0,0 +1,44 @@
+package com.example.weatherforecast.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+
+}
